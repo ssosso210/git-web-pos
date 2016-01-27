52a9bacab57e01c22ddf914eee522c95c18d3923
euny1993 <eunyoungpppp@gmail.com>
2016-01-27 17:33:33 +0900

    fdf

---
+++ git-web-pos/src/main/java/com/dobbypos/model/service/MenuService.java
@@ -0,0 +1,9 @@
+package com.dobbypos.model.service;
+
+import com.dobbypos.model.dto.Menu;
+import com.dobbypos.model.dto.StoreTable;
+
+public interface MenuService {
+
+	void insertMenu(Menu menu);
+}
