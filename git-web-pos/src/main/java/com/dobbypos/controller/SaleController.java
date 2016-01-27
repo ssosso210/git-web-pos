2b8c9cd2ed9ff45530cc0718010ffd705494bb48
STU5 <STU5@STU5-PC>
2016-01-27 21:32:37 +0900

    메뉴가져오기

--- git-web-pos/src/main/java/com/dobbypos/controller/SaleController.java
+++ git-web-pos/src/main/java/com/dobbypos/controller/SaleController.java
@@ -1,13 +1,17 @@
 package com.dobbypos.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.dobbypos.model.dao.EmployeeDao;
 import com.dobbypos.model.dto.Employee;
+import com.dobbypos.model.dto.Menu;
 import com.dobbypos.model.service.EmployeeService;
 import com.dobbypos.model.service.SaleService;
 
@@ -26,44 +30,22 @@ public class SaleController {
 		return "sale/salehome"; 
 	}
 	
-	
-	@RequestMapping(value = "/orderform.action", method = RequestMethod.GET)
-	public String orderForm() {
-		return "sale/orderform"; 
-	}	
-	
 	@RequestMapping(value = "/paymentform.action", method = RequestMethod.GET)
-	public String paymentForm() {
+	public String PaymentForm() {
 		return "sale/paymentform"; 
 	}	
 	
-	@RequestMapping(value = "/menulist.action", method = RequestMethod.POST)
-	public String EmployeeRegister(Employee employee) {
-	//	employee.setPasswd(Util.getHashedString(member.getPasswd(), "SHA-1"));
-//		System.out.println(employee.toString());
-//		saleService.insertEmployee(employee);
+	@RequestMapping(value = "/orderform.action", method = RequestMethod.GET)
+	public String SelectAllMenu(Model model) {
+		
+		List<Menu> menus = saleService.getAllMenus();
+	    model.addAttribute("menus", menus);
+		
 		System.out.println(" [ 메뉴가져왔어 ]  ");
-		return "redirect:/sale/salehome.action";
+		return "sale/orderform";
 	}
 	
-	@RequestMapping(value="/tableregisterform.action", method=RequestMethod.GET)
-	public String TableRegisterForm(){
-		return "settings/tableregisterform";
-	}
-	@RequestMapping(value="/tableregister.action", method=RequestMethod.POST)
-	public String TableRegister(){
-		System.out.println("table register 성공 ");
-		return "redirect:/settings/settinghome.action";
-	}
-	
-	
-	
-	/*//employeer 등록 취소시
-	@RequestMapping(value = "/list.action", method = RequestMethod.GET)
-	public String EmployeeList() {
-		return "settings/employeelist"; 
-	}*/
-	
+
 	
 	
 }
