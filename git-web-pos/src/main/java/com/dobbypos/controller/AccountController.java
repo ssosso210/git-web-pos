163c37dadee24fcccdf82291cb2982b46d76a771
STU6 <STU6@STU6-PC>
2016-01-27 18:02:59 +0900

    d

---
+++ git-web-pos/src/main/java/com/dobbypos/controller/AccountController.java
@@ -0,0 +1,87 @@
+package com.dobbypos.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dobbypos.common.Util;
+import com.dobbypos.model.dto.Employee;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+	
+	@Autowired
+	@Qualifier("employeeService")
+	private EmployeeService employeeService;
+
+	@RequestMapping(value = "login.action", method = RequestMethod.GET)
+	public String loginForm() {
+		
+		return "account/loginform"; // /WEB-INF/views/ + account/loginform + .jsp
+	}	
+	
+	@RequestMapping(value = "login.action", method = RequestMethod.POST)
+	public String login(
+		HttpSession session, HttpServletRequest req,
+		String hqCode,String storeCode, String employeeId, String passwd, @RequestParam("returnurl") String returnUrl) {
+		
+		
+		boolean resultValue= false;
+		
+		passwd = Util.getHashedString(passwd, "SHA-1");
+		
+		
+		//요청 데이터 (아이디, 열쇠글)으로 데이터베이스에서 조회
+
+		Employee employee = employeeService.searchEmployeeByLogin(hqCode,storeCode, employeeId, passwd);
+
+		//조회 결과에 따라 이동 처리
+		if (employee != null) {
+			if(employee.getPasswd().equals(passwd)){
+				resultValue = true;
+			}
+		}
+		
+		
+		
+		
+		//조회 결과에 따라 이동 처리
+		if (resultValue) {
+			System.out.println(employee.toString());
+			session.setAttribute("loginuser", employee);//로그인 처리
+			if (returnUrl != null && returnUrl.length() > 0) {
+				//spring mvc에서 redirect 경로는 application이름을 포함할 수 없습니다.
+				return "redirect:" + returnUrl.replace("/dobbywebpos", "");
+			} else {
+				return "redirect:/main.action";
+			}
+		} else {
+			
+			//request객체에 데이터 저장
+			req.setAttribute("loginid", employeeId);
+			return "account/loginform"; 
+		}
+		
+	}	
+	
+	@RequestMapping(value = "logout.action", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+		
+		session.removeAttribute("loginuser");
+		
+		return "redirect:/account/login.action";
+	}
+	
+}
+
+
+
+
+
---
+++ git-web-pos/src/main/java/com/dobbypos/controller/SettingsController.java
@@ -0,0 +1,68 @@
+package com.dobbypos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dobbypos.model.dao.EmployeeDao;
+import com.dobbypos.model.dto.Employee;
+import com.dobbypos.model.service.EmployeeService;
+
+@Controller
+@RequestMapping("/settings")
+public class SettingsController {
+	
+
+	@Autowired
+	@Qualifier("employeeService")
+	private EmployeeService employeeService;
+	
+	@RequestMapping(value = "/settinghome.action", method = RequestMethod.GET)
+	public String SettingMenu() {
+		
+		return "settings/settinghome"; 
+	}
+	
+	
+	@RequestMapping(value = "/employeeregisterform.action", method = RequestMethod.GET)
+	public String EmployeeRegisterForm() {
+		return "settings/employeeregisterform"; 
+	}	
+	
+	@RequestMapping(value = "/employeeregister.action", method = RequestMethod.POST)
+	public String EmployeeRegister(Employee employee) {
+	//	employee.setPasswd(Util.getHashedString(member.getPasswd(), "SHA-1"));
+		System.out.println(employee.toString());
+		employeeService.insertEmployee(employee);
+		System.out.println("employee register 성공  ");
+		return "redirect:/settings/settinghome.action";
+	}
+	
+	@RequestMapping(value="/tableregisterform.action", method=RequestMethod.GET)
+	public String TableRegisterForm(){
+		return "settings/tableregisterform";
+	}
+	@RequestMapping(value="/tableregister.action", method=RequestMethod.POST)
+	public String TableRegister(){
+		System.out.println("table register 성공 ");
+		return "redirect:/settings/settinghome.action";
+	}
+	
+	
+	
+	/*//employeer 등록 취소시
+	@RequestMapping(value = "/list.action", method = RequestMethod.GET)
+	public String EmployeeList() {
+		return "settings/employeelist"; 
+	}*/
+	
+	
+	
+}
+
+
+
+
+
